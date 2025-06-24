0258ebc42de1813dfc0f85952f1fbfef7d0b40ce
Chukwufumnaya <naomiokelue2002@gmail.com>
2025-06-24 12:26:04 +0100

    Add initial implementation of weather app with CSS, HTML, and JavaScript
    - Created weather.css for styling the application with responsive design.
    - Developed weather.html structure including search bar and weather display sections.
    - Implemented weather.js to handle API calls for current weather and five-day forecast.
    - Integrated Google Custom Search API for dynamic background images based on city input.

---
+++ api-key.js
@@ -0,0 +1,4 @@
+export const apiKey = 'ec13494ca5518a3354b3705c76dafda3';
+
+export const googleApikey = 'AIzaSyB4UeWm_X0SokHKWMyPHWlSRiAHh1PSy0Q';
+export const googleSearchEngineId = 'a146d043ffd6f42ed&q';
